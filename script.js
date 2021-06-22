eb5b8d832206b13f2d6e26d08ff4609892958e48
CodeGuy02 <86216164+CodeGuy02@users.noreply.github.com>
2021-06-22 05:48:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function compute()
 {
     p = document.getElementById("principal").value;
     
-    document.getElementById("result").value = "test value test value : " + p;
+    document.getElementById("result").innerHTML = " TEST " + p;
     
 }
         
