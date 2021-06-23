98a75fea39dc963ad14846202a46f207349e601a
CodeGuy02 <86216164+CodeGuy02@users.noreply.github.com>
2021-06-22 21:43:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+    interest = document.getElementById("myRange").value;
+    years = document.getElementById("years").value;
     
-    document.getElementById("result").innerHTML = " TEST " + p;
+    interestCalculation = principal * interest * years; 
+    lineA = "If you deposit " + principal ", ";
+    lineB = "at an interest rate of " + interest + "% ." 
+    lineC = "You will receive an amount of " + interestCalculation + ",";
+    lineD = "in the year" + new Date().getFullYear();
+    
+    document.getElementById("resultA").innerHTML = lineA;
+    document.getElementById("resultB").innerHTML = lineB;
+    document.getElementById("resultC").innerHTML = lineC;
+    document.getElementById("resultD").innerHTML = lineD;
     
 }
         
