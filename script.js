7fc3cbe6e8b19d016cbd4a5aad6c03d149e410d6
CodeGuy02 <86216164+CodeGuy02@users.noreply.github.com>
2021-06-23 04:24:22 -0400

    seperate regular text from highlights

--- script.js
+++ script.js
@@ -5,17 +5,25 @@ function compute()
     years = document.getElementById("years").value;
     
     interestCalculation = principal * (interest / 100) * years; 
-    lineA = "If you deposit " + principal.toString() + ", ";
-    lineB = "at an interest rate of " + interest.toString() + "% ." 
-    lineC = "You will receive an amount of " + interestCalculation.toString() + ",";
+    lineA = "If you deposit ";
+    lineA_highlight = principal.toString() + ", ";
+    lineB = "at an interest rate of ";
+    lineB_highlight = interest.toString() + "% .";
+    lineC = "You will receive an amount of ";
+    lineC_highlight = interestCalculation.toString() + ",";
     currentYear = new Date().getFullYear();
     finalYear = currentYear + parseInt(years);
     <!-- alert(" typeof currentYear: " + typeof currentYear + " typeof years: " + typeof years + " typeof finalYear: " + typeof finalYears); -->
-    lineD = "in the year " + finalYear.toString();
+    lineD = "in the year ";
+    lineD_highlight = finalYear.toString();
     
     document.getElementById("resultA").innerHTML = lineA;
+    document.getElementById("resultA_highlight").innerHTML = lineA_highlight;
     document.getElementById("resultB").innerHTML = lineB;
+    document.getElementById("resultB_highlight").innerHTML = lineB_highlight;
     document.getElementById("resultC").innerHTML = lineC;
+    document.getElementById("resultC_highlight").innerHTML = lineC_highlight;
     document.getElementById("resultD").innerHTML = lineD;
+    document.getElementById("resultD_highlight").innerHTML = lineD_highlight;
     
 }
